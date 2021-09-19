34e99c3153fe4a16da8c5f2c6575da6db1ad1505
girdacio <girdacio@gmail.com>
2021-09-18 22:46:31 -0300

    manda dados da sessao qdo novo client socket conecta

--- index.js
+++ index.js
@@ -14,9 +14,51 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected');  
+  
+  setupSession(socket);
+  sendUsers(socket);  
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  
+});
+
+io.use((socket, next) => {
+  console.log('middleware');
+  const { retroId } = socket.handshake.query;
+
+  if (!retroId) {
+    return next(new Error("NO_RETRO"));
+  }
+
+  next();
 });
 
 httpServer.listen(porta, () => {
   console.log(`listening on *:${porta}`);
 });
+
+function setupSession(socket) {
+  let { retroId, userId } = socket.handshake.query;
+  
+  socket.join(retroId);
+  
+  userId = userId != "undefined" ? userId : socket.id;
+  const sessionData = {
+    userId: userId
+  }
+
+  socket.emit('session', sessionData);
+}
+
+function sendUsers(socket) {
+  const users = [];
+
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push(id);
+  }
+
+  socket.emit("users", users);
+}
